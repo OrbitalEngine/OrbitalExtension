8ffe849fe1b9c1cdf13bb4061c373ffce6024703
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-07-08 09:53:16 +0600

    add Vector2f.rem and use one-liner

--- src/main/kotlin/dev/yeff/orbital/extensions/Vector2fExtensions.kt
+++ src/main/kotlin/dev/yeff/orbital/extensions/Vector2fExtensions.kt
@@ -4,18 +4,12 @@ import dev.yeff.orbital.math.Vector2f;
 
 // Binary Operators
 
-operator fun Vector2f.plus(second: Vector2f): Vector2f {
-    return Vector2f(this.x + second.x, this.y + second.y)
-}
+operator fun Vector2f.plus(second: Vector2f): Vector2f = Vector2f(this.x + second.x, this.y + second.y)
 
-operator fun Vector2f.minus(second: Vector2f): Vector2f {
-    return Vector2f(this.x - second.x, this.y - second.y)
-}
+operator fun Vector2f.minus(second: Vector2f): Vector2f = Vector2f(this.x - second.x, this.y - second.y)
 
-operator fun Vector2f.times(second: Vector2f): Vector2f {
-    return Vector2f(this.x * second.x, this.y * second.y)
-}
+operator fun Vector2f.times(second: Vector2f): Vector2f = Vector2f(this.x * second.x, this.y * second.y)
 
-operator fun Vector2f.div(second: Vector2f): Vector2f {
-    return Vector2f(this.x / second.x, this.y / second.y)
-}
+operator fun Vector2f.div(second: Vector2f): Vector2f = Vector2f(this.x / second.x, this.y / second.y)
+
+operator fun Vector2f.rem(second: Vector2f): Vector2f = Vector2f(this.x % second.x, this.y % second.y)
