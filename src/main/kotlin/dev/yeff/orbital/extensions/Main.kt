339daa96830cd6765cc2550018f5e2be0f6e44bc
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-07-09 14:47:51 +0600

    add unary and postfix operators

--- src/main/kotlin/dev/yeff/orbital/extensions/Main.kt
+++ src/main/kotlin/dev/yeff/orbital/extensions/Main.kt
@@ -3,14 +3,17 @@ package dev.yeff.orbital.extensions
 import dev.yeff.orbital.math.Vector2f
 
 fun main() {
-    val first = Vector2f(100.0f, 100.0f)
-    val second = Vector2f(20.0f, 20.0f);
-    val third = Vector2f(100.0f, 100.0f)
+    var first = Vector2f(100.0f, 100.0f)
+    var second = Vector2f(20.0f, 20.0f);
+    var third = Vector2f(100.0f, 100.0f)
 
     println(first + second)
     println(first - second)
     println(first * second)
     println(first / second)
-    println(first == third)
+    println(-first)
+    first++
+    println(first)
+    first--
+    println(first)
 }
-
--- src/main/kotlin/dev/yeff/orbital/extensions/Vector2fExtensions.kt
+++ src/main/kotlin/dev/yeff/orbital/extensions/Vector2fExtensions.kt
@@ -13,3 +13,13 @@ operator fun Vector2f.times(second: Vector2f): Vector2f = Vector2f(this.x * seco
 operator fun Vector2f.div(second: Vector2f): Vector2f = Vector2f(this.x / second.x, this.y / second.y)
 
 operator fun Vector2f.rem(second: Vector2f): Vector2f = Vector2f(this.x % second.x, this.y % second.y)
+
+// Unary operators
+
+operator fun Vector2f.unaryMinus(): Vector2f = Vector2f(-this.x, -this.y)
+
+// Increment and Decrement
+
+operator fun Vector2f.inc(): Vector2f = Vector2f(this.x + 1, this.y + 1)
+
+operator fun Vector2f.dec(): Vector2f = Vector2f(this.x - 1, this.y - 1)
